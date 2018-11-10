baca86f6f00b4cf64894606d57a3d9b3ef0f352d
strdr4605 <str.dr4605@gmail.com>
2018-11-10 18:28:50 +0200

    add simple login form to http://172.31.235.1:8000/api/v1/login

--- src/components/inbox/Inbox.js
+++ src/components/inbox/Inbox.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './Inbox.css'
 
 import Mail from '../mail';
+import Login from '../login';
 
 class Inbox extends Component {
 	render() {
 		return (
 			<div className = "inbox">
+        <Login/>
 				<Mail/>
 			</div>
 		)
