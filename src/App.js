7ee7863645db22da8f0d0d8961798becb476d18b
Dragoș Străinu <str.dr4605@gmail.com>
2018-11-10 16:52:35 +0200

    Applied suggestions
    Co-Authored-By: viorelrj <viorelrinja@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Inbox from './components/inbox';
-import Kanban from './components/kanban/';
+import Kanban from './components/kanban';
 
 class App extends Component {
 	render() {
